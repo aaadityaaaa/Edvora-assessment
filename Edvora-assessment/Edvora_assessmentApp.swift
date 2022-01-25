813536271cbee27fc3c568d285121fe6b956360c
aaadityaaaa <aaadityaaazz@gmail.com>
2022-01-25 15:15:35 +0530

    Initial commit.

---
+++ Edvora-assessment/Edvora_assessmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  Edvora_assessmentApp.swift
+//  Edvora-assessment
+//
+//  Created by Aaditya Singh on 25/01/22.
+//
+
+import SwiftUI
+
+@main
+struct Edvora_assessmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
